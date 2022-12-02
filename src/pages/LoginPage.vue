fc73158d0c07bea9d897fdf302779ade22b14e3b
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-12-01 18:26:35 -0600

    Escribir la template para el formulario de inicio de sesión

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,11 +1,51 @@
 <template>
   <q-page padding>
-    Login
+    <div class="q-pa-md q-mt-lg">
+      <q-bar dark>
+        <p class="text-h4">Acceder</p>
+      </q-bar>
+
+      <q-form
+        @submit="login"
+        class="q-gutter-md"
+      >
+        <q-input
+          type="password"
+          v-model="form.password"
+          label="Contraseña"
+          :hint="loginHintComputed"
+          lazy-rules
+          filled
+          :rules="[
+            val => val && val.length > 0 || 'Campo requerido'
+          ]"
+        />
+
+        <q-btn
+          label="Iniciar sesión"
+          type="submit"
+          color="primary"
+        />
+      </q-form>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
   name: 'LoginPage',
+  methods: {
+    login () {
+      if (this.$store.state.auth.firstAccess) {
+        this.successLogin();
+      } else {
+        if (this.form.password === this.$store.state.auth.access) {
+          this.successLogin();
+        } else {
+          this.wrongLogin();
+        }
+      }
+    }
+  }
 }
 </script>
